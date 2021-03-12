3770ee5375ddaa2fdf941f5627c8085c9a919b61
Eric Yu <49173353+ery010@users.noreply.github.com>
2021-03-11 17:32:41 -0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ function Check(x) {
 	var checked_radios = document.querySelectorAll('input[type=radio]:checked');
 	var nbr_checked_radios = document.querySelectorAll('input[type=radio]:checked').length;
 
-	if (nbr_checked_radios == 5) {
+	if (nbr_checked_radios == 1) {
 		button.disabled = false;
 	}
 	form.addEventListener("submit", function(event) {
@@ -20,35 +20,72 @@ function Check(x) {
 		  // output = output + entry[0] + "=" + entry[1] + "\r";
 		  output = output + entry[1] + "\r";
 		};
-	    //log.innerText = nbr_checked_radios;
+	    // log.innerText = nbr_checked_radios;
 	    // log.innerText = checked_radios[0].value;
 		event.preventDefault();
 	  }, false);
 }
 
-console.log()
-
 function makePlot() {
-	var button = document.getElementById("submit");
-	var form = document.querySelector("form");
-	var log = document.querySelector("#log");
+	// var button = document.getElementById("submit");
+	// var form = document.querySelector("form");
+	// var log = document.querySelector("#log");
 	var checked_radios = document.querySelectorAll('input[type=radio]:checked');
+	img1 = document.getElementById("image1");
+	img2 = document.getElementById("image2");
+	img3 = document.getElementById("image3");
+	console.log(img1)
 	
-	var chk1 = checked_radios[0].value // 10, 25, 40
-	var chk2 = checked_radios[1].value // no mask, n95, 40
-	var chk3 = checked_radios[2].value // 1, 3, 5
-	var chk4 = checked_radios[3].value // 3, 5, 7
-	var chk5 = checked_radios[4].value // yes, no
+	var chk = checked_radios[0].value // low, mid, high
+	console.log(checked_radios[0].value)
+	if (chk == "low") {
+		img1.style.visibility = "visible";
+		img1.style.display = "inline-block"
 
-	var pointSize = 3
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
+		img2.style.visibility = "hidden";
+		img2.style.display = "none"
+
+		img3.style.visbility = "hidden";
+		img3.style.display = "none"
+
+		console.log(img1)
+		
+	} else if (chk == "mid") {
+		img2.style.visibility = "visible";
+		img2.style.display = "inline-block"
+
+		img1.style.visibility = "hidden";
+		img1.style.display = "none"
+
+		img3.style.visbility = "hidden";
+		img3.style.display = "none"
+
+	} else {
+		img3.style.visibility = "visible";
+		img3.style.display = "inline-block"
+
+		img2.style.visibility = "hidden";
+		img2.style.display = "none"
+
+		img1.style.visbility = "hidden";
+		img1.style.display = "none"
+	}
 	
-	// for (const x of)
-	ctx.fillStyle = "#000000";
-	ctx.beginPath();
-	ctx.arc(160, 275, 3, 0, 2 * Math.PI);
-	ctx.fill();
+
+	// // var chk2 = checked_radios[1].value // no mask, n95, 40
+	// // var chk3 = checked_radios[2].value // 1, 3, 5
+	// // var chk4 = checked_radios[3].value // 3, 5, 7
+	// // var chk5 = checked_radios[4].value // yes, no
+
+	// var pointSize = 3
+	// var c = document.getElementById("myCanvas");
+	// var ctx = c.getContext("2d");
+	
+	// // for (const x of)
+	// ctx.fillStyle = "#000000";
+	// ctx.beginPath();
+	// ctx.arc(160, 275, 3, 0, 2 * Math.PI);
+	// ctx.fill();
 }
 
 // document.addEventListener('DOMContentLoaded', init, false);
